af80dbdafd5b646b50058cef7223a84dbc9566a2
leizhang <stonenot@gmail.com>
2011-07-04 19:12:18 -0700

    Remove admin block from default blocks

--- config.php
+++ config.php
@@ -7,6 +7,6 @@
 //
 // The default blocks layout for this course format:
     $format['defaultblocks'] = 'menu_site_and_course,activity_modules,search_forums,'.
-                               'admin,course_list:news_items,calendar_month,calendar_upcoming,'.
+                               'course_list:news_items,calendar_month,calendar_upcoming,'.
                                'recent_activity';
 //
