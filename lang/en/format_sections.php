367d52234d710b459285d325b4248acd8a47b438
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-08-15 15:42:42 +1200

    Add missing strings (showfromothers, hidefromothers)

--- lang/en/format_sections.php
+++ lang/en/format_sections.php
@@ -3,3 +3,5 @@ $string['formatsections']='Single Sections'; // Name to display for format
 $string['namesections']='Section'; // Name of a section within your format
 $string['section0name']='Section'; // Name of a section within your format
 $string['pluginname']='Single Sections';
+$string['hidefromothers'] = 'Hide section';
+$string['showfromothers'] = 'Show section';
