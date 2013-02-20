9de3aabb20931af01ad7e7c43b7e723efe16b720
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-02-20 14:06:54 +1300

    Make sure all sections are created
    Sections were not being created when the number of sections was increased.

--- format.php
+++ format.php
@@ -18,6 +18,10 @@ if ($topic = optional_param('topic', 0, PARAM_INT)) {
 }
 // End backwards-compatible aliasing..
 
+// make sure all sections are created
+$course = course_get_format($course)->get_course();
+course_create_sections_if_missing($course, range(0, $course->numsections));
+
 // Remember page for the duration of the session.
 $home = optional_param('home', 0, PARAM_INT);
 if (!empty($home) || !isset($_SESSION['current_topic_'.$course->id])) {
