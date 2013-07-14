9b66fb42b1821c7960fe8189b0dcc43ec49f23dd
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-07-15 11:51:25 +1200

    Take user to requested section even if no section is set in session
    If no section is set in the session (i.e. it's the first time the user
    is accessing this course in this session), the section number passed
    in the URL was being ignored and the session's section number set to 0.
    This resulted in users being taken to the course homepage even if they
    were attempting to get to a specific section; clicking the link again
    would take them to the section.

--- format.php
+++ format.php
@@ -24,7 +24,7 @@ course_create_sections_if_missing($course, range(0, $course->numsections));
 
 // Remember page for the duration of the session.
 $home = optional_param('home', 0, PARAM_INT);
-if (!empty($home) || !isset($_SESSION['current_topic_'.$course->id])) {
+if (!empty($home)) {
     $section = 0;
     $_SESSION['current_topic_'.$course->id] = 0;
 } else {
